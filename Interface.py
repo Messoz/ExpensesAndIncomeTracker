2a12adeea1b42862a84165940736d0c08c0e9a2e
MarcoMessora <marcomessora9@gmail.com>
2022-09-26 16:36:50 +0200

    Now it is work but it can be improved

--- Interface.py
+++ Interface.py
@@ -1,73 +1,218 @@
 import tkinter as tk
-from PIL import ImageTk, Image
+import tkinter
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from tkinter import ttk
 import tracker 
+import time
+
+from PIL import ImageTk, Image
+from tkinter import ttk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from tracker import DATES, PRICES, GOODS_OR_SERVICES, EXPENSE_TYPES
+from tracker import DATES, PRICES, GOODS_OR_SERVICES, EXPENSE_TYPES, EQUITY, DATES_EQ
+
+def create_df_ex():
+   df=tracker.CreateDataFrame_ex()
+   df1=pd.DataFrame()
+   try:
+      df['DATES']=pd.to_datetime(df['DATES'], dayfirst = True , format = '%d/%m/%Y')
+      df1=df.sort_values(by='DATES')
+   except:
+      root = tk.Tk()
+      label = tk.Label(root, text='DATE PRICE NOT VALID',
+                 font=('Times New Roman','35'), fg='red', bg='white')
+      label.pack()
+      root.overrideredirect(True)
+      root.geometry("+300+400")
+      root.wm_attributes("-topmost", True)
+      root.wm_attributes("-disabled", True)
+      root.wm_attributes("-transparentcolor", "white")
+
+      root.after(1000, root.destroy)
+
+   return df1
+
+def create_df_in():
+   df=tracker.CreateDataFrame_in()
+   df1=pd.DataFrame()
+   try:
+      df['DATES_STIP']=pd.to_datetime(df['DATES_STIP'], dayfirst = True , format = '%d/%m/%Y')
+      df1=df.sort_values(by='DATES_STIP')
+   except:
+      root = tk.Tk()
+      label = tk.Label(root, text='DATE NOT VALID',
+                 font=('Times New Roman','35'), fg='red', bg='white')
+      label.pack()
+      root.overrideredirect(True)
+      root.geometry("+300+400")
+      root.wm_attributes("-topmost", True)
+      root.wm_attributes("-disabled", True)
+      root.wm_attributes("-transparentcolor", "white")
+
+      root.after(1000, root.destroy)
+
+   return df1
+#create the section where is possible to see the lasts expenses
 
 def show_df():
-   page2 = tk.Frame(root)
-   page2.place(relwidth= 0.5, relheight =0.8, relx=0.45, rely=0.1)
-   myLabel1 = tk.Label(page2,text="LAST EXPENSES")
-   myLabel1.pack()
-   df=tracker.CreateDataFrame()
+   df_table = tk.Frame(root) 
+   df_table.place(relwidth= 0.5, relheight =0.8, relx=0.45, rely=0.1)
+
+   #l1 = tk.Label(df_table,text="LAST EXPENSES") 
+   #l1.pack()
+   df=create_df_ex()
+   #df=tracker.CreateDataframe_ex_ex()
    cols = list(df.columns)
-   tree = ttk.Treeview(page2, show='headings', height=8)
+   tree = ttk.Treeview(df_table, show='headings', height=15)
    tree.pack()
    tree["columns"] = cols
    for i in cols:
-      tree.column(i, anchor="w",width=100)
+      tree.column(i, anchor="w",width=120)
       tree.heading(i, text=i, anchor='w')
 
    for index, row in df.iterrows():
-      tree.insert("",0,text=index,values=list(row))
+      tree.insert("", 0, text=index, values=list(row))
 
-def p1_action_CONFIRMbutton(e1,e2,e3,e4):
+   #label space
+   ls = tk.Label(df_table,text=" ")
+   ls.pack()
+   l5 = tk.Label(df_table,text="ENTER YOUR EQUITY: (€)").pack()
+   ls = tk.Label(df_table,text=" ")
+   ls.pack()
+   e5=tk.Entry(df_table, width=20, borderwidth = 5)
+   e5.pack()
+   ls = tk.Label(df_table,text=" ")
+   ls.pack()
+   b1=tk.Button(df_table,text='CONFIRM EQUITY',padx=10, pady=5, fg='white',bg="blue", command= lambda : action_equity_button(e5)).pack()
+
+def action_equity_button(e5):
+   equity=e5.get()   
+   tracker.setEquity(float(equity),EQUITY,DATES_EQ)
+   e5.delete(0,tk.END)
+
+
+
+def show_df_in():
+   df_table = tk.Frame(root) 
+   df_table.place(relwidth= 0.5, relheight =0.8, relx=0.45, rely=0.1)
+
+   #l1 = tk.Label(df_table,text="LAST INCOMES") 
+   #l1.pack()
+   df=create_df_in()
+   #df=tracker.CreateDataframe_ex_ex()
+   cols = list(df.columns)
+   tree = ttk.Treeview(df_table, show='headings', height=8)
+   tree.pack()
+   tree["columns"] = cols
+   for i in cols:
+      tree.column(i, anchor="w",width=150)
+      tree.heading(i, text=i, anchor='w')
+
+   for index, row in df.iterrows():
+      tree.insert("", 0, text=index, values=list(row))
+# when the CONFIRM button is pushed ..
+
+def p1_action_CONFIRMbutton(page,e1,e2,e3,e4):
    goodStr=e1.get()
    priceStr=e2.get()
    dateStr=e3.get()
    typeStr=e4.get()
-   tracker.addExpenses(goodStr,float(priceStr),dateStr,typeStr)
+   try:
+      priceStr=float(priceStr)
+      tracker.addExpenses(goodStr,priceStr,dateStr,typeStr)
+   except:
+      root = tk.Tk()
+      label = tk.Label(root, text='VALUE PRICE NOT VALID',
+                 font=('Times New Roman','35'), fg='red', bg='white')
+      label.pack()
+
+      root.overrideredirect(True)
+      root.geometry("+300+400")
+      root.wm_attributes("-topmost", True)
+      root.wm_attributes("-disabled", True)
+      root.wm_attributes("-transparentcolor", "white")
+
+      root.after(1000, root.destroy)
+      #page.after(1000, lambda: l.delete(0, tk.END))
+   
    e1.delete(0,tk.END)
    e2.delete(0,tk.END)
    e3.delete(0,tk.END)
    e4.delete(0,tk.END)
 
-def show_Expenses(page):
-   df=tracker.CreateDataFrame()
+def p1_action_CONFIRMbutton_in(page,e1,e2,e3):
+   typestr=e1.get()
+   moneystr=e2.get()
+   dateStr=e3.get()
+
+   try:
+      moneyStr=float(moneyStr)
+      tracker.addIncomes(typestr,moneystr,dateStr)
+   except:
+      root = tk.Tk()
+      label = tk.Label(root, text='VALUE MONEY NOT VALID',
+                 font=('Times New Roman','35'), fg='red', bg='white')
+      label.pack()
+
+      root.overrideredirect(True)
+      root.geometry("+300+400")
+      root.wm_attributes("-topmost", True)
+      root.wm_attributes("-disabled", True)
+      root.wm_attributes("-transparentcolor", "white")
+
+      root.after(1000, root.destroy)
+      #page.after(1000, lambda: l.delete(0, tk.END))
+   
+   e1.delete(0,tk.END)
+   e2.delete(0,tk.END)
+   e3.delete(0,tk.END)
+
+
+def show_Graphs(page):
+   df=tracker.CreateDataFrame_ex()
    fig=tracker.graphs(df)
    figure1 =fig[0]
    page_fig1 = tk.Frame(root)
-   page_fig1.place(relwidth= 0.3, relheight =0.3, relx=0.1, rely=0.1,anchor="nw")
+   page_fig1.place(relwidth= 0.37, relheight =0.37, relx=0, rely=0,anchor="nw")
    canvas1 = FigureCanvasTkAgg(figure1, page_fig1)
    canvas1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
+   figure4 =fig[3]
+   page_fig4 = tk.Frame(root)
+   page_fig4.place(relwidth= 0.37, relheight =0.43, relx=1, rely=0,anchor="ne")
+   canvas4 = FigureCanvasTkAgg(figure4, page_fig4)
+   canvas4.get_tk_widget().pack(side=tk.LEFT,fill=tk.BOTH,expand=1)
+
+   figure3 =fig[2]
+   page_fig3 = tk.Frame(root)
+   page_fig3.place(relwidth= 0.37, relheight =0.43, relx=0, rely=0.37,anchor="nw")
+   canvas3 = FigureCanvasTkAgg(figure3, page_fig3)
+   canvas3.get_tk_widget().pack(side=tk.LEFT,fill=tk.BOTH,expand=1)
+"""
    figure2 =fig[1]
    page_fig2 = tk.Frame(root)
-   page_fig2.place(relwidth= 0.3, relheight =0.3, relx=0.1, rely=0.4,anchor="nw")
+   page_fig2.place(relwidth= 0.37, relheight =0.43, relx=0, rely=0.4,anchor="nw")
    canvas2 = FigureCanvasTkAgg(figure2, page_fig2)
    canvas2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
-   
-
 """
    
-"""
 
+   
+def delete_EX():
+   tracker.deleteLastExpense()
+def delete_in():
+   tracker.deleteLastIncome()
 
 def page1(root):
-
-   window_width  = 1000
+   window_width  = 1100
    window_height = 600
    #create the inner canvas 
    canvas = tk.Canvas(root, height= window_height, width=window_width, bg='#263D42')
    canvas.pack()
 
    page = tk.Frame(root)
-   page.place(relwidth= 0.3, relheight =0.8, relx=0.1, rely=0.1)
+   page.place(relwidth= 0.3, relheight =0.85, relx=0.05, rely=0.05)
    # title
    l=tk.Label(page, text = "INSERT THE EXPENSES", fg="red").pack()
 
@@ -96,7 +241,6 @@ def page1(root):
    e3.pack()
    dateStr=e3.get()
    
-
    #insert TYPE
    myLabel4 = tk.Label(page,text="TYPE")
    e4=tk.Entry(page, width=20, borderwidth = 5)
@@ -109,73 +253,170 @@ def page1(root):
    ls = tk.Label(page,text=" ")
    ls.pack()
 
+   df_table = tk.Frame(root) 
+   df_table.place(relwidth= 0.5, relheight =0.85, relx=0.45, rely=0.05)
+
+   l1 = tk.Label(df_table,text="LAST EXPENSES") 
+   l1.pack()
+   show_df()
+   
    #CONFIRM expense button
-   exit_button = tk.Button(page,text='CONFIRM',padx=10, pady=5, fg='white',bg="blue", command= lambda : [p1_action_CONFIRMbutton(e1,e2,e3,e4),show_df()])
+   exit_button = tk.Button(page,text='CONFIRM',padx=10, pady=5, fg='white',bg="blue", command= lambda : [p1_action_CONFIRMbutton(page,e1,e2,e3,e4),show_df()])
    exit_button.pack()
+
+  #label space
+   ls = tk.Label(page,text=" ")
+   ls.pack()
   
+   #DELETE LAST EXPENSE
+   delete_button = tk.Button(page,text='DELETE LAST EXPENSES',padx=30, pady=5, fg='white',bg="red", command= lambda : [delete_EX(),show_df()])
+   delete_button.pack()
+
+   #exit button
+   exit_button = tk.Button(page,text='Exit',command=lambda: page.quit(),padx=10, pady=5, fg='white',bg="#263D42")
+   exit_button.pack(side = 'bottom',fill=tk.X)
+
+   #SHOW INCOME
+   b1=tk.Button(page, text = '-->', command = lambda : [changepage(2),show_df_in()] , padx=10, pady=5,fg='white',bg="#263D42")
+   b1.pack(side = 'bottom',fill=tk.X)
+
+   #SHOW GRAPHS
+   b1=tk.Button(page, text = '<--', command = lambda : changepage(3),padx=10, pady=5,fg='white',bg="#263D42")
+   b1.pack(side = 'bottom',fill=tk.X)
+
+   
+
+
+def page2(root):
+   window_width  = 1100
+   window_height = 600
+   #create the inner canvas 
+   canvas = tk.Canvas(root, height= window_height, width=window_width, bg='#263D42')
+   canvas.pack()
+
+   page = tk.Frame(root)
+   page.place(relwidth= 0.3, relheight =0.85, relx=0.05, rely=0.05)
+   # title
+   l=tk.Label(page, text = "INSERT THE INCOMES", fg="green").pack()
+
+   #insert TYPE
+   myLabel1 = tk.Label(page,text="TYPE")
+   e1=tk.Entry(page, width=20, borderwidth = 5)
+   
+   myLabel1.pack()
+   e1.pack()
+   goodStr=e1.get()
+
+   #insert MONEY
+   myLabel2 = tk.Label(page,text="MONEY (€)")
+   e2=tk.Entry(page, width=20, borderwidth = 5)
+   
+   myLabel2.pack()
+   e2.pack()
+   priceStr=e2.get()
+
+   #insert DATE
+   myLabel3 = tk.Label(page,text="DATE (dd/mm/yyyy)")
+   e3=tk.Entry(page, width=20, borderwidth = 5)
+
+   myLabel3.pack()
+   e3.pack()
+   dateStr=e3.get()
+
+   #label space
+   ls = tk.Label(page,text=" ")
+   ls.pack()
+
+   df_table = tk.Frame(root) 
+   df_table.place(relwidth= 0.5, relheight =0.85, relx=0.45, rely=0.05)
+
+   l1 = tk.Label(df_table,text="LAST INCOMES") 
+   l1.pack()
+
+
+   #CONFIRM expense button
+   exit_button = tk.Button(page,text='CONFIRM',padx=10, pady=5, fg='white',bg="blue", command= lambda : [p1_action_CONFIRMbutton_in(page,e1,e2,e3),show_df_in()])
+   exit_button.pack()
+
+  #label space
+   ls = tk.Label(page,text=" ")
+   ls.pack()
+  
+   #DELETE LAST EXPENSE
+   delete_button = tk.Button(page,text='DELETE LAST INCOMES',padx=30, pady=5, fg='white',bg="green", command= lambda : [delete_in(),show_df_in()])
+   delete_button.pack()
 
    #exit button
    exit_button = tk.Button(page,text='Exit',command=lambda: page.quit(),padx=10, pady=5, fg='white',bg="#263D42")
    exit_button.pack(side = 'bottom',fill=tk.X)
 
    #SHOW LAST EXPENSES
-   b1=tk.Button(page, text = 'SHOW LAST EXPENSES', command = show_df, padx=10, pady=5,fg='white',bg="#263D42")
-   b1.pack(side = 'bottom',fill=tk.X)
+   #b1=tk.Button(page, text = 'SHOW LAST INCOMES', command = show_df_in, padx=10, pady=5,fg='white',bg="#263D42")
+   #b1.pack(side = 'bottom',fill=tk.X)
 
    #SHOW GRAPHS
-   b1=tk.Button(page, text = 'SHOW GRAPHS', command = changepage,padx=10, pady=5,fg='white',bg="#263D42")
+   b1=tk.Button(page, text = '-->', command = lambda : changepage(3),padx=10, pady=5,fg='white',bg="#263D42")
    b1.pack(side = 'bottom',fill=tk.X)
 
-   
+   #SHOW INCOME
+   b1=tk.Button(page, text = '<--', command =lambda : [changepage(1),show_df()], padx=10, pady=5,fg='white',bg="#263D42")
+   b1.pack(side = 'bottom',fill=tk.X)
 
-   
 
-
-
-def page2(root):
-   window_width  = 1000
+def page3(root):
+   window_width  = 1100
    window_height = 600
 
    #create the inner canvas 
    canvas = tk.Canvas(root, height= window_height, width=window_width, bg='#263D42')
-   canvas.pack()
-
+   canvas.place(relwidth= 1, relheight =1, relx=1, rely=1,anchor="ne")
    page = tk.Frame(root)
-   page.place(relwidth= 0.8, relheight =0.8, relx=0.1, rely=0.1)
+   show_Graphs(page)
 
-   # title
-   #l=tk.Label(page, text = "GRAPHS", fg="red")
-   #l.pack()
-
-   # exit button
-   refresh_button = tk.Button(page,text='Refresh',command=show_Expenses(page),padx=10, pady=5, fg='white',bg="#263D42")
-   refresh_button.pack()
-
-   # exit button
-   exit_button = tk.Button(page,text='Exit',command=lambda: root.quit(),padx=10, pady=5, fg='white',bg="#263D42")
+    # exit button
+   exit_button = tk.Button(root,text='Exit',command=lambda: root.quit(),padx=10, pady=5, fg='white',bg="#263D42")
    exit_button.pack(side = 'bottom', fill=tk.X)
 
    #To page 1
-   b1=tk.Button(page, text = 'INSERT EXPENSES', command = changepage,padx=10, pady=5, fg='white',bg="#263D42")
+   b1=tk.Button(root, text = '-->', command = lambda : [changepage(1),show_df()],padx=10, pady=5, fg='white',bg="#263D42")
    b1.pack(side = 'bottom', fill=tk.X)
 
+   #SHOW INCOME
+   b2=tk.Button(root, text = '<--', command =lambda :[changepage(2),show_df_in()], padx=10, pady=5,fg='white',bg="#263D42")
+   b2.pack(side = 'bottom',fill=tk.X)
 
-def changepage():
-    global pagenum, root
-    for widget in root.winfo_children():
-        widget.destroy()
-    if pagenum == 1:
-        page2(root)
-        pagenum = 2
-    else:
-        page1(root)
-        pagenum = 1
+
+
+def changepage(n):
+   global pagenum, root
+   for widget in root.winfo_children():
+      widget.destroy()
+   if pagenum == 1 and n==2:
+     page2(root)
+     pagenum = 3
+   if pagenum == 1 and n==3:
+     page3(root)
+     pagenum = 3
+
+   if pagenum == 2 and n==1:
+     page1(root)
+     pagenum = 1
+   if pagenum == 2 and n==3:
+     page3(root)
+     pagenum = 3
+
+   if pagenum == 3 and n==1:
+     page1(root)
+     pagenum = 1
+   if pagenum == 3 and n==2:
+     page2(root)
+     pagenum = 2
 
 
 pagenum = 1
 root = tk.Tk()
 root.title('Expense Tracker DEMO')
-window_width  = 1000
+window_width  = 1100
 window_height = 600
 #root.geometry( str(window_width) + 'x' + str(window_height))
 #root.resizable(0, 0)
@@ -194,4 +435,5 @@ root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 root.iconbitmap('C:/Users/messo/Documents/MyProject_Tracker2/Salaryico.ico')
 
 page1(root)
+root.resizable(True, True)
 root.mainloop()
--- tracker.py
+++ tracker.py
@@ -1,117 +1,179 @@
 # This projects aim to create an appplication for tracking expenses 
-
+import tkinter as tk
 import numpy as np
 import datetime as date
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
+import matplotlib.dates as mdates
+import numpy as np
+from time import strftime,localtime
+import datetime as dt
 from tabulate import tabulate
 
-#Create empty lists
+# I decide to not use objected oriented programming but this could be possible development
+# Create empty list where equity is tracked
+EQUITY=[]
+DATES_EQ=[]
+#Create empty lists EXPENSES
 GOODS_OR_SERVICES = []
 PRICES =[]
 DATES =[]
 EXPENSE_TYPES = []
+#Create empty lists EXPENSES
+TYPES_STIP = []
+MONEY =[]
+DATES_STIP=[]
 
-# Create a function to add to list an expenses
+# def reset EQUITY function
+def resetEquity ():
+	EQUITY.clear()
+	DATES_EQ.clear()
+# def set EQUITY function 
+def setEquity (equity,EQUITY,DATES_EQ):
+	x=dt.datetime.now()
+	DATES_EQ.append(x.strftime("%d/%m/%Y"))
+	EQUITY.append(float(equity))
+	df=CreateDataFrame_EQ()
+	EQUITY=df['EQUITY'].values.tolist()
+	DATES_EQ=df['DATES_EQ'].values.tolist()
+	
+# def delete LAST EXPENSE
+def deleteLastExpense ():
+	GOODS_OR_SERVICES.pop()
+	PRICES.pop()
+	DATES.pop()
+	EXPENSE_TYPES.pop()
+	EQUITY.pop()
+	DATES_EQ.pop()
+
+def deleteLastIncome ():
+	TYPES_STIP.pop()
+	MONEY.pop()
+	DATES_STIP.pop()
+	EQUITY.pop()
+	DATES_EQ.pop()
+
 def addExpenses (good_or_service,price,date,expense_type):
 	GOODS_OR_SERVICES.append(good_or_service)
 	PRICES.append(price)
 	DATES.append(date)
 	EXPENSE_TYPES.append(expense_type)
+	EQUITY.append(float(-price))
+	DATES_EQ.append(date)
+		
+def addIncomes (type_stip,money_stip,date_stip):
+	TYPES_STIP.append(type_stip)
+	MONEY.append(money_stip)
+	DATES_STIP.append(date_stip)
+	EQUITY.append(float(money_stip))
+	DATES_EQ.append(date_stip)
 
-def CreateDataFrame ():
+def CreateDataFrame_ex ():
 	expense_report = pd.DataFrame()
 	expense_report['GOODS'] = GOODS_OR_SERVICES
 	expense_report['PRICES'] = PRICES
 	expense_report['DATES'] = DATES
 	expense_report['EXPENSE_TYPES'] = EXPENSE_TYPES
+	expense_report['DATES']=pd.to_datetime(expense_report['DATES'], dayfirst = True , format = '%d/%m/%Y')
 	sorted_expense_report = expense_report.sort_values(by='DATES')
-	print(tabulate(expense_report, headers='keys', tablefmt='psql'))
-	return expense_report
+	print(tabulate(sorted_expense_report, headers='keys', tablefmt='psql'))
+	return sorted_expense_report
 
+def CreateDataFrame_in ():
+	income_report = pd.DataFrame()
+	income_report['TYPES'] = TYPES_STIP
+	income_report['MONEY'] = MONEY
+	income_report['DATES_STIP'] = DATES_STIP
+	income_report['DATES_STIP']=pd.to_datetime(income_report['DATES_STIP'], dayfirst = True , format = '%d/%m/%Y')
+	sorted_income_report = income_report.sort_values(by='DATES_STIP')
+	print(tabulate(sorted_income_report, headers='keys', tablefmt='psql'))
+	return sorted_income_report
 
-def dataFrameTest_Year():
-	num_of_ex = 100
+def CreateDataFrame_EQ ():
+	eq_report = pd.DataFrame()
+	eq_report['EQUITY'] = EQUITY
+	eq_report['DATES_EQ'] = DATES_EQ
+	eq_report['DATES_EQ']=pd.to_datetime(eq_report['DATES_EQ'], dayfirst = True,format = '%d/%m/%Y')
+	sorted_eq_report = eq_report.sort_values(by='DATES_EQ')
+	print(tabulate(sorted_eq_report, headers='keys', tablefmt='psql'))
+	return sorted_eq_report
+
+def dataFrameTest_Year_ex():
+	num_of_ex = 10
 	Goods=["Mela","Pera","Banana","Carne","Acqua","cena","PC","telefono"]
 	Price=[5,5,10,15,0.5,40,800,1000]
 	Expenses_type=["Alimentari","Tecnologia"]
 	for i in range(num_of_ex):
-		date=str(random.randint(1,28))+"/"+ str(random.randint(1,12)) + "/"+"2022"
+		date=str(random.randint(25,28))+"/"+ str(random.randint(9,12)) + "/"+"2022"
 		addExpenses(Goods[random.randint(0,7)],Price[random.randint(0,7)],date,Expenses_type[random.randint(0,1)])
-	df = CreateDataFrame()
-	df['DATES']=pd.to_datetime(df['DATES'], dayfirst = True , format = '%d/%m/%Y', exact=True)
-	df1=df.sort_values(by='DATES')
-	return df1
+	df = CreateDataFrame_ex()
+	CreateDataFrame_EQ ()
+	return df
+
+def dataFrameTest_Year_in():
+	num_of_ex = 4
+	type_stip=["Lavoro","azioni"]
+	Money=[1600,400]
+	for i in range(num_of_ex):
+		date=str(random.randint(1,23))+"/"+ str(random.randint(1,9)) + "/"+"2022"
+		addIncomes(type_stip[random.randint(0,1)],Money[random.randint(0,1)],date)
+	df = CreateDataFrame_in()
+	CreateDataFrame_EQ ()
+	return df
 
 def graphs(df):
-	fig1=plt.figure(figsize=(3, 3), dpi=50)
-	plt.pie(df['PRICES'],labels=df["EXPENSE_TYPES"], shadow=True, rotatelabels=True)
+	fig1=plt.figure(figsize=(3, 3), dpi=100)
+	df1=pd.DataFrame()
+	df1=df.groupby(["EXPENSE_TYPES"] , as_index=False)['PRICES'].sum()
+	plt.pie(df1['PRICES'],labels=df1["EXPENSE_TYPES"], shadow=True, textprops = dict(color ="red"))
+	plt.title("EXPENSES TYPES")
 
-	fig2=plt.figure(figsize=(3, 3), dpi=50)
-	plt.scatter(df['GOODS'],df["PRICES"])
-	return fig1,fig2
+	fig2=plt.figure(figsize=(3, 3), dpi=100)
+	plt.bar(df['GOODS'],df["PRICES"],color='red')
+	plt.title("EXPENSES GOODS")
+
+	fig3=plt.figure(figsize=(7, 5), dpi=100)
+	df2=df.tail(20)
+	now = dt.datetime.now()
+	then = now - dt.timedelta(days=31)
+	days = mdates.drange(then,now,dt.timedelta(days=1))
+	#plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
+	#plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=3))
+	df5=CreateDataFrame_in()
+	plt.plot(df2['DATES'],df2['PRICES'],'o',color='red')
+	plt.plot(df5['DATES_STIP'],df5['MONEY'],'o',color='green')
+	plt.gcf().autofmt_xdate()
+	plt.title("INCOME-EXPENSES")
+
+	fig4=plt.figure(figsize=(3, 3), dpi=100)
+	df1=CreateDataFrame_EQ()
+	eq=[]
+	eq_sum=[]
+	dat=[]
+	eq=df1['EQUITY'].values.tolist()
+	s=eq[0]
+	for i in range(1,len(eq)+1):
+		for j in range(0,i):
+			s=s+eq[j]
+		eq_sum.append(s)
+		s=0
+	print(eq_sum)
+	dat=df1['DATES_EQ'].values.tolist()
+	df2=df1.loc[df1['EQUITY'] > 0]
+	df3=df1.loc[df1['EQUITY'] < 0]
+	plt.bar(df2['DATES_EQ'],df2['EQUITY'],color='green')
+	plt.bar(df3['DATES_EQ'],df3['EQUITY'],color='red')
+	plt.plot(df1['DATES_EQ'],eq_sum,"blue")
+	plt.gcf().autofmt_xdate()
+	plt.title("EQUITY")
+
+	return fig1,fig2,fig3,fig4
 	
+x=[]
+table_ex=dataFrameTest_Year_ex()
+table_in=dataFrameTest_Year_in()
+for i in range(10,210,10):
+		x.append(int(i))
+print(x)
 
-
-
-
-
-"""
-# This function return 1 if date1 in string format dd/mm/yyyy is after date2
-#				return 0 if date1 in string format dd/mm/yyyy is equal date2
-#				return -1 if date1 in string format dd/mm/yyyy is equal date2
-def Date_isgreater(Date1,Date2):
-	strDate1= Date1.split("/") #parsing date 1
-	strDate2= Date2.split("/") #parsing date 2
-	years= int(strDate1[2]) > int(strDate2[2])
-	yearsAndMonth = int(strDate1[2]) == int(strDate2[2]) and int(strDate1[1]) > int(strDate2[1])
-	yearsAndMonthAndDay = int(strDate1[2]) == int(strDate2[2]) and int(strDate1[1]) == int(strDate2[1]) and int(strDate1[0]) > int(strDate2[0])
-	if  years or yearsAndMonth or yearsAndMonthAndDay:
-		return 1
-	elif (int(strDate1[2]) == int(strDate2[2]) and int(strDate1[1]) == int(strDate2[1]) and int(strDate1[0]) == int(strDate2[0])):
-		return 0
-	else:
-		return -1
-
-def orderDate(DateList):
-	for i  in range(len(DateList)-1):
-		if Date_isgreater(DateList[i],DateList[i+1]) == 1:
-			DateList[i],DateList[i+1] = DateList[i+1],DateList[i]
-	return DateList
-"""
-
-
-#todaydate= date.datetime.now()
-#addExpenses('Mela',int('1'),todaydate.strftime("%d/%d/%Y"),'Alimentari')
-#addExpenses('Banana',int('2'),todaydate.strftime("%d/%d/%Y"),'Alimentari')
-#ShowExpenses()
-
-table_ex=dataFrameTest_Year()
-print(tabulate(table_ex, headers='keys', tablefmt='psql'))
-#order_DATES=orderDate(table_ex.loc[:,"DATE"])
-#order_DATES=orderDate(["12/2/2012","19/09/1998","20/09/1998"])
-#print(order_DATES)
-
-start_date = "1/1/2022"
-end_date = "30/1/2022"
-df2=table_ex.loc[(table_ex["DATES"] >= start_date) & (table_ex["DATES"] <= end_date)]
-df2['DATES']=pd.to_datetime(df2['DATES'], dayfirst=False, format="%d/%m/%Y")
-print(tabulate(df2, headers='keys', tablefmt='psql'))
-df2.plot(kind='bar',x='DATES',y="PRICES")
-plt.show()
-
-"""
-
-ET_plot0 = table_ex[['EXPENSE_TYPE','PRICE']]
-
-ET_plot=ET_plot0.groupby(['EXPENSE_TYPE']).sum()
-print(tabulate(ET_plot, headers='keys', tablefmt='psql'))
-
-ET_plot.plot.bar()
-plt.show()
-#plt.plot(x='GOODS',y="PRICE")
-#plt.show()
-# Main program
-
-"""
